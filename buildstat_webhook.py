b3faf56e6916de40e91342ad6bb0c1b576ec7c79
Emeka Apugo <Emeka.Apugo@symphony.com>
2017-02-01 18:23:19 -0500

    call internally

--- buildstat_webhook.py
+++ buildstat_webhook.py
@@ -7,8 +7,8 @@ BUILD_NUMBER = os.environ['BUILD_NUMBER']
 url = "https://corporate.symphony.com/integration/v1/whi/simpleWebHookIntegration/5810d144e4b0f884b709cc90/58752050e4b06ace62af4947"
 text = "project build error is here: %s, %s, %s" % (BUILD_URL, JOB_NAME, BUILD_NUMBER)
 mention = '<mention email="emeka.apugo@symphony.com"/>'
-Build.result = "SUCCESS"
-result = "project status is a", Build.result
+Build_result = "SUCCESS"
+result = "project status is a", Build_result
 payload =  '<messageML>\n %s \n %s \n %s \n!!!</messageML>' % (text, mention, result)
 
 headers = {

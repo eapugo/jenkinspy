87a6dc25d24654c426477f2501a3d7f9233b09c3
Emeka Apugo <Emeka.Apugo@symphony.com>
2017-02-01 17:28:36 -0500

    checkmeout

---
+++ setuppyenv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#install python venv on jenkins slave
+
+PYENV_HOME=$WORKSPACE/.saltpyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Create virtualenv and install necessary packages
+virtualenv --no-site-packages $PYENV_HOME
+. $PYENV_HOME/bin/activate
+pip install salt-pepper
+pip install request

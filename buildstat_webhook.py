3776aa56e52e73c53be58ac0d338b430a0a8876a
Emeka Apugo <Emeka.Apugo@symphony.com>
2017-02-01 19:02:01 -0500

    correct mention syntax

--- buildstat_webhook.py
+++ buildstat_webhook.py
@@ -1,4 +1,3 @@
-#!/bin/python
 import requests
 import os
 BUILD_URL = os.environ['BUILD_URL']
@@ -6,16 +5,22 @@ JOB_NAME = os.environ['JOB_NAME']
 BUILD_NUMBER = os.environ['BUILD_NUMBER']
 url = "https://corporate.symphony.com/integration/v1/whi/simpleWebHookIntegration/5810d144e4b0f884b709cc90/58752050e4b06ace62af4947"
 text = "project build error is here: %s, %s, %s" % (BUILD_URL, JOB_NAME, BUILD_NUMBER)
-mention = '<mention email="emeka.apugo@symphony.com"/>'
+mention = "Emeka.Apugo@symphony.com"
 Build_result = "SUCCESS"
-result = "project status is a", Build_result
-payload =  '<messageML>\n %s \n %s \n %s \n!!!</messageML>' % (text, mention, result)
-
+result = "project status is a %s" % (Build_result)
+payload =  '<messageML>\n %s \n %s \n %s!!!\n</messageML>' % (text, mention, result)
+print payload
 headers = {
     'content-type': "text/plain",
     'cache-control': "no-cache",
+    'user-agent': 'my-app/0.0.1',
     }
 
-response = requests.post(url, data=payload, headers=headers)
+r = requests.post(url, data=payload, headers=headers)
 
-print(response.text)
+print(r)
+print
+print
+print
+print r.status_code
+print r.headers
